d231189f77c723efc678581a679b92a2ef5d43d9
AndreaFerrara13 <134604849+AndreaFerrara13@users.noreply.github.com>
2023-07-12 16:44:19 +0200

    esercizio

---
+++ js.js
@@ -0,0 +1,24 @@
+const inserirenome = (submitEvents) => {
+  inserirenome.preventDefault();
+  const form = submitEvents.target;
+  const inputlist = form.elements;
+
+  const name = {};
+  for (let i = 0; i < inputlist.length; i++) {
+    if (inputlist[i].tagName != "BUTTON") {
+      const valoreInput = inputlist[i].value;
+      const id = inputlist[i].id;
+      name[idInput] = valoreInput;
+    }
+  }
+};
+console.log(name);
+const conferma = confirm("confermi?");
+if (conferma) {
+  const conferma = "GRAZIE DI ESSERTI REGISTRATO!";
+  const container = document.querySelector(".registrato");
+  const li = document.createElement("li");
+  li.innerText = `${name.nome}`;
+  `${name.password}`;
+  container.appendChild(li);
+}
