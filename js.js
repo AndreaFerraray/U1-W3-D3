08087ea2d99ee4b729aa926b97844621821ceceb
AndreaFerrara13 <134604849+AndreaFerrara13@users.noreply.github.com>
2023-07-12 22:54:45 +0200

    giusto

--- js.js
+++ js.js
@@ -1,25 +1,8 @@
-const inserirenome = (submitEvents) => {
-  inserirenome.preventDefault();
-  const form = submitEvents.target;
-  const inputlist = form.elements;
+const addtask = (submitevent) => {
+  submitevent.preventDefault();
 
-  const name = {};
-  for (let i = 0; i < inputlist.length; i++) {
-    if (inputlist[i].tagName != "BUTTON") {
-      const valoreInput = inputlist[i].value;
-      const id = inputlist[i].id;
-      name[idInput] = valoreInput;
-    }
-  }
+  const input = document.querySelector("input#task");
+  console.log(input.value);
 };
-console.log(name);
-const conferma = confirm("confermi?");
-if (conferma) {
-  const container = document.querySelector(".registrato");
-  const li = document.createElement("li");
-  li.innerHTML = `${name.nome}`;
-  `${name.password}`;
-  container.appendChild(li);
-  console.log("okay");
-  li.innerText = "GRAZIE DI ESSERTI REGISTRATO!";
-}
+const ul = document.querySelector("ul");
+ul.innerHTML += `<li> ${input.value} </li>`;
